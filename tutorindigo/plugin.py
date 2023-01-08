605fd0f3247feedbaed2d09901522de4b2ec83c0
Zameel Hassan <43750093+zameel7@users.noreply.github.com>
2023-01-08 15:46:33 +0530

    Update plugin.py

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -22,8 +22,8 @@ config = {
         "FOOTER_LEGAL_LINKS": [
             {"title": "Terms of service", "url": "/tos"},
             {
-                "title": "Indigo theme for Open edX",
-                "url": "https://github.com/overhangio/tutor-indigo",
+                "title": "NISH theme for Open edX",
+                "url": "https://github.com/NISH-COURSES/tutor-indigo",
             },
         ],
     },
